9bce2207ca2776c363239cf82e18b993666dcbf6
Naveed <naveedalianwr@gmail.com>
2020-06-19 16:08:40 +0500

    Created isMobile function

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ document.addEventListener("contextmenu", function (event) {
 const audio = new Audio('../audio/audio.mp3');
 window.onload = audio.play().then(audio.volume = 0.55).then(audio.loop = true);
 
-// IsMobile
+// IsMobile Function
 function isMobile() {
     let isMobile = false;
     if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) ||
